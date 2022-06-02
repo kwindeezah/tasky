b29b373ec25880bf18419c2b0ef6a8bc5853d300
Hadizat Oluwaseun <hadizathmotunrayo@gmail.com>
2022-06-02 13:10:11 +0100

    deleted some comments

--- index.php
+++ index.php
@@ -2,19 +2,6 @@
 include ("config.php");
 ?>
 
-	<!-- // check if submit button is clicked
-	// if (isset($_POST['submit'])) {
-	// 	if (empty($_POST['task'])) {
-	// 		$errors = "Fill in the task!";
-	// 	}else{
-	// 		$task = $_POST['task'];
-	// 		$sql = "INSERT INTO tasks (task) VALUES ('$task')";
-	// 		mysqli_query($conn, $sql);
-    //         header('location: index.php');
-	// 	}
-	// }	 --> 
-  
-
 <html>
       <head>
           <title>ToDo List App</title>
